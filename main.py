14cc62b7b627a1d916e6558a029667969925fd83
Italo Costa <italocostajobs@gmail.com>
2022-10-22 17:31:40 -0300

    Feat: Create Usuario Class

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from xml.dom.minidom import Identified
 import psycopg2 as db
+
+errorString = "Error:"
 class Config:
     def __init__(self):
         self.configs = {
@@ -11,6 +14,7 @@ class Config:
             }
         }
 
+
 class Connection(Config):
     def __init__(self):
         Config.__init__(self)
@@ -18,14 +22,15 @@ class Connection(Config):
             self.conn = db.connect(**self.configs["postgres"])
             self.cur = self.conn.cursor()
         except Exception as e:
-            print("Error:", e)
+            print("Config", errorString, e)
             exit(1)
+
     def __enter__(self):
-      return self
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-      self.commit()
-      self.connection.close()
+        self.commit()
+        self.connection.close()
 
     @property
     def connection(self):
@@ -35,26 +40,58 @@ class Connection(Config):
     def cursor(self):
         return self.cur
 
-    @property
     def commit(self):
         return self.connection.commit()
 
     def fetchall(self):
-      return self.cursor.fetchall()
+        return self.cursor.fetchall()
 
     def execute(self, sql, params=None):
-      self.cursor.execute(sql, params or ())
+        self.cursor.execute(sql, params or ())
 
     def query(self, sql, params=None):
-      self.cursor.execute(sql, params or ())
-      return self.fetchall()
+        self.cursor.execute(sql, params or ())
+        return self.fetchall()
 
-class Person(Connection):
-  def __init__(self):
-    Connection.__init__(self)
-    
 
-conexao = db.connect(
+class Usuario(Connection):
+    def __init__(self):
+        Connection.__init__(self)
+
+    def create(self, *args):
+        try:
+            sql = """CREATE TABLE IF NOT EXISTS usuario(
+                  name varchar(100),
+                  cpf char(11),
+                  password varchar(20) UNIQUE,
+                  street varchar(80),
+                  district varchar(40),
+                  number int,
+                  tel_number int,
+                  user_type varchar(40),
+                  PRIMARY KEY(cpf)
+                )"""
+            self.execute(sql, args)
+            self.commit()
+        except Exception as e:
+            print("Create", errorString, e)
+
+    def insert(self, *args):
+        try:
+            sql = "INSERT INTO usuario (name, cpf) VALUES (%s, %s)"
+            self.execute(sql, args)
+            self.commit()
+        except Exception as e:
+            print("Insert", errorString, e)
+
+
+if __name__ == "__main__":
+    usuario = Usuario()
+    usuario.create()
+    usuario.insert(
+        "Italo", '12345678912')
+
+""" conexao = db.connect(
     host="localhost",
     database="teste",
     port=5432,
@@ -70,4 +107,4 @@ result = cursor.fetchall()
 
 # Navega pelo resultset
 for record in result:
-    print(record[0], "-->", record[1])
+    print(record[0], "-->", record[1]) """
